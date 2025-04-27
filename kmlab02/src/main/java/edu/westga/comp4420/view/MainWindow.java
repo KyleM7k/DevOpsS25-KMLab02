74241bf54cbd0d80dcf35402d5ce93fcd703a72d
Kyle Meadows <km00406@my.westga.edu>
2025-04-27 13:08:08 -0400

    Updated UI
    Updated UI, beginning code behind now

--- kmlab02/src/main/java/edu/westga/comp4420/view/MainWindow.java
+++ kmlab02/src/main/java/edu/westga/comp4420/view/MainWindow.java
@@ -1,6 +1,11 @@
-package edu.westga.comp4420.kmlab02.view;
+package edu.westga.comp4420.view;
 
+import edu.westga.comp4420.model.ShoppingList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class MainWindow {
@@ -8,4 +13,32 @@ public class MainWindow {
     @FXML
     private AnchorPane MainWindow;
 
+    @FXML
+    private Label itemNameLbl;
+
+    @FXML
+    private TextField itemNameTxtBox;
+
+    @FXML
+    private Label itemQuantLbl;
+
+    @FXML
+    private TextField itemQuantTxtBox;
+
+    @FXML
+    private ListView<ShoppingList> shoppingListView;
+
+    @FXML
+    private Button submitBtn;
+
+    @FXML
+    private Button updateBtn;
+
+    @FXML
+    private Label welcomeLbl;
+    
+    @FXML
+    void initalize() {
+    	
+    }
 }
