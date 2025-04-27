b61b60cf0a4c994f7b2f76ebc715dec894172b94
KyleM7k <149911309+KyleM7k@users.noreply.github.com>
2025-04-26 20:23:26 -0400

    Added Project Files
    Added the project files to the directory

---
+++ kmlab02/src/main/java/edu/westga/comp4420/view/MainWindow.java
@@ -0,0 +1,11 @@
+package edu.westga.comp4420.kmlab02.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class MainWindow {
+
+    @FXML
+    private AnchorPane MainWindow;
+
+}
